4713d98387b46f65b0eb4b29b5a7b99074654a2a
chengganghu <chenglaian@gmail.com>
2017-12-08 12:17:33 +0800

    add the description of the problem

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -1,7 +1,12 @@
 //
 // Created by ChenggangHu on 17/12/8.
 //
-
+/*
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
 #include "AddTwoNumbers.h"
 #include <iostream>
 ListNode* AddTwoNumbers::addTwoNumbers(ListNode *l1, ListNode *l2) {
