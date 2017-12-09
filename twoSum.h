9d62e8d0fe642c324e0c0df7620f0830888c84eb
chengganghu <chenglaian@gmail.com>
2017-12-09 12:59:34 +0800

    twoSum problems

--- twoSum.h
+++ twoSum.h
@@ -1,7 +1,10 @@
 //
 // Created by ChenggangHu on 17/12/9.
 //
-
+/*
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
 #ifndef LEETCODE_TWOSUM_H
 #define LEETCODE_TWOSUM_H
 
